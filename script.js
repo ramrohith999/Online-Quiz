d895f456a3125d9fa2657e40062a49eca466e378
Ram Rohith Maringanti <ramrohith999@gmail.com>
2021-08-17 18:07:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 (function() {
   var questions = [{
     question: "What is the capital of France?",
-    choices: [Moscow, Berlin , Paris, Barcelona, Sydny],
+    choices: ['Moscow', 'Berlin', 'Paris', 'Barcelona', 'Sydny'],
     correctAnswer: 3
   }, {
     question: "Where is Niagara water falls located?",
-    choices: [USA, Brazil, Indonesia, India, Canada],
+    choices: ['USA', 'Brazil', 'Indonesia', 'India', 'Canada'],
     correctAnswer: 5
   }, {
     question: "What is 8*9?",
