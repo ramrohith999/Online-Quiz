da51dc9d2b7296a2282cfb2b48abdd9ea3417c28
Ram Rohith Maringanti <ramrohith999@gmail.com>
2021-08-17 18:24:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,21 +24,13 @@
   var questionCounter = 0;
   var selections = [];
   var quiz = $('#quiz');
-  
-  // Display initial question
   displayNext();
-  
-  // Click handler for the 'next' button
   $('#next').on('click', function (e) {
     e.preventDefault();
-    
-    // Suspend click listener during fade animation
     if(quiz.is(':animated')) {        
       return false;
     }
     choose();
-    
-    // If no user selection, progress is stopped
     if (isNaN(selections[questionCounter])) {
       alert('Please make a selection!');
     } else {
@@ -46,8 +38,6 @@
       displayNext();
     }
   });
-  
-  // Click handler for the 'prev' button
   $('#prev').on('click', function (e) {
     e.preventDefault();
     
@@ -58,8 +48,6 @@
     questionCounter--;
     displayNext();
   });
-  
-  // Click handler for the 'Start Over' button
   $('#start').on('click', function (e) {
     e.preventDefault();
     
@@ -71,17 +59,12 @@
     displayNext();
     $('#start').hide();
   });
-  
-  // Animates buttons on hover
   $('.button').on('mouseenter', function () {
     $(this).addClass('active');
   });
   $('.button').on('mouseleave', function () {
     $(this).removeClass('active');
   });
-  
-  // Creates and returns the div that contains the questions and 
-  // the answer selections
   function createQuestionElement(index) {
     var qElement = $('<div>', {
       id: 'question'
@@ -98,8 +81,6 @@
     
     return qElement;
   }
-  
-  // Creates a list of the answer choices as radio inputs
   function createRadios(index) {
     var radioList = $('<ul>');
     var item;
@@ -113,13 +94,9 @@
     }
     return radioList;
   }
-  
-  // Reads the user selection and pushes the value to an array
   function choose() {
     selections[questionCounter] = +$('input[name="answer"]:checked').val();
   }
-  
-  // Displays next requested element
   function displayNext() {
     quiz.fadeOut(function() {
       $('#question').remove();
@@ -130,8 +107,6 @@
         if (!(isNaN(selections[questionCounter]))) {
           $('input[value='+selections[questionCounter]+']').prop('checked', true);
         }
-        
-        // Controls display of 'prev' button
         if(questionCounter === 1){
           $('#prev').show();
         } else if(questionCounter === 0){
@@ -148,8 +123,6 @@
       }
     });
   }
-  
-  // Computes score and returns a paragraph element to be displayed
   function displayScore() {
     var score = $('<p>',{id: 'question'});
     
