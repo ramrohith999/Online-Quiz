5a9192d098541f902d307776752e01149ff7ac23
Ram Rohith Maringanti <ramrohith999@gmail.com>
2021-08-17 18:35:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 (function() {
   var questions = [{
-    question: "What is the capital of France?",
-    choices: [MOSCOW, BERLIN, PARIS, BARCALONA, SYDNEY],
+    question: "What is 2*5?",
+    choices: [2, 5, 10, 15, 20],
     correctAnswer: 2
   }, {
-    question: "Where is Niagara water falls located?",
-    choices: [USA, BRAZIL, INDONESIA, INDIA, CANADA],
+    question: "What is 3*6?",
+    choices: [3, 6, 9, 12, 18],
     correctAnswer: 4
-  },{
+  }, {
     question: "What is 8*9?",
     choices: [72, 99, 108, 134, 156],
     correctAnswer: 0
@@ -21,16 +21,24 @@
     correctAnswer: 4
   }];
   
-  var questionCounter = 0;
-  var selections = [];
-  var quiz = $('#quiz');
+  var questionCounter = 0; //Tracks question number
+  var selections = []; //Array containing user choices
+  var quiz = $('#quiz'); //Quiz div object
+  
+  // Display initial question
   displayNext();
+  
+  // Click handler for the 'next' button
   $('#next').on('click', function (e) {
     e.preventDefault();
+    
+    // Suspend click listener during fade animation
     if(quiz.is(':animated')) {        
       return false;
     }
     choose();
+    
+    // If no user selection, progress is stopped
     if (isNaN(selections[questionCounter])) {
       alert('Please make a selection!');
     } else {
@@ -38,6 +46,8 @@
       displayNext();
     }
   });
+  
+  // Click handler for the 'prev' button
   $('#prev').on('click', function (e) {
     e.preventDefault();
     
@@ -48,6 +58,8 @@
     questionCounter--;
     displayNext();
   });
+  
+  // Click handler for the 'Start Over' button
   $('#start').on('click', function (e) {
     e.preventDefault();
     
@@ -59,16 +71,22 @@
     displayNext();
     $('#start').hide();
   });
+  
+  // Animates buttons on hover
   $('.button').on('mouseenter', function () {
     $(this).addClass('active');
   });
   $('.button').on('mouseleave', function () {
     $(this).removeClass('active');
   });
+  
+  // Creates and returns the div that contains the questions and 
+  // the answer selections
   function createQuestionElement(index) {
     var qElement = $('<div>', {
       id: 'question'
     });
+    
     var header = $('<h2>Question ' + (index + 1) + ':</h2>');
     qElement.append(header);
     
@@ -80,6 +98,8 @@
     
     return qElement;
   }
+  
+  // Creates a list of the answer choices as radio inputs
   function createRadios(index) {
     var radioList = $('<ul>');
     var item;
@@ -93,9 +113,13 @@
     }
     return radioList;
   }
+  
+  // Reads the user selection and pushes the value to an array
   function choose() {
     selections[questionCounter] = +$('input[name="answer"]:checked').val();
   }
+  
+  // Displays next requested element
   function displayNext() {
     quiz.fadeOut(function() {
       $('#question').remove();
@@ -106,6 +130,8 @@
         if (!(isNaN(selections[questionCounter]))) {
           $('input[value='+selections[questionCounter]+']').prop('checked', true);
         }
+        
+        // Controls display of 'prev' button
         if(questionCounter === 1){
           $('#prev').show();
         } else if(questionCounter === 0){
@@ -122,6 +148,8 @@
       }
     });
   }
+  
+  // Computes score and returns a paragraph element to be displayed
   function displayScore() {
     var score = $('<p>',{id: 'question'});
     
